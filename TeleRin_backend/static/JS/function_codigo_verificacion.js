92a2fed44710c2ecd1c722cb92f63c6e86ccb59e
Alejandro DS <ducuarasatizzabalalejandro@gmail.com>
2026-04-09 14:10:01 -0500

    archivos js y html ya pasados borrados

--- TeleRin_backend/static/JS/function_codigo_verificacion.js
+++
@@ -1,9 +0,0 @@
-import { enviarInfoServer } from "./function_generales.js"
-
-const formularioCodigoVerifiacion = document.getElementById("formularioCodigoVerifiacion")
-formularioCodigoVerifiacion.addEventListener("submit", (e)=>{
-
-    const codigo = document.getElementById("codigo_usuario").value
-    const direccion_ = `/validar_codigo`
-    enviarInfoServer(e, {"codigo": codigo}, direccion_, "json_mensaje")
-})
