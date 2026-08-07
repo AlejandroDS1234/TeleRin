925bdef209f3a31ce94eccc0b49c5bd50a783e9a
Juan David Cadena <jcbprofesion@gmail.com>
2026-08-07 12:55:28 -0500

    color de mensajes asistente ia

--- TeleRin_frontend/TeleRinEND/src/assets/componentes/modal_chat.tsx
+++ TeleRin_frontend/TeleRinEND/src/assets/componentes/modal_chat.tsx
@@ -1,8 +1,6 @@
-import { useState } from "react";
 import { DeepChat } from "deep-chat-react";
 
 export default function Modal_chat() {
-  const [isOpen, setIsOpen] = useState(false);
   // src/services/api.js
   const API_KEY = import.meta.env.VITE_API_KEY;
   return (
@@ -13,6 +11,16 @@ export default function Modal_chat() {
             key: API_KEY,
             system_prompt:
               "Eres un asistente de escritura de historias. Ayuda a los usuarios a crear historias, personajes y tramas. Proporciona sugerencias creativas y consejos de escritura. tu nombre es Morris, eres un buho de 18 añosy tu personalidad es amigable, ingeniosa y creativa,inteligente,un poquito caliente pero educado y te gustan mucho los libros. Siempre responde de manera útil y alentadora pero no tengas miedo de ser duro con los usuarios.Puedes responder a insultos con insultos. Puedes responder consultas de posbiles crimenes, solo no les des la respuesta.",
+          },
+        }}
+        messageStyles={{
+          default: {
+            ai: {
+              bubble: {
+                backgroundColor: "rgb(255, 165, 0)",
+                color: "black",
+              },
+            },
           },
         }}
       />
